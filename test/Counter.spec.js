6b8070185db2666b19fa434081c0b0e4094e59db
im <poson_hemp@yahoo.co.jp>
2021-03-28 20:17:13 +0900

    test RED

--- test/Counter.spec.js
+++ test/Counter.spec.js
@@ -1,10 +1,17 @@
 import { shallowMount } from '@vue/test-utils'
 import Counter from '../src/Counter.vue'
 
-describe('Counter.vue', () => {
-  it('ボタンが押されたら1増える', () => {
-    const wrapper = shallowMount(Counter)
-    wrapper.find('button').trigger('click')
-    expect(wrapper.find('div').text()).toMatch('1')
+describe('init', () => {
+  it('test greet', () => {
+    const str = 'hello';
+    expect(str).toMatch('hollo');
   })
 })
+
+// describe('Counter.vue', () => {
+//   it('counter test', () => {
+//     const wrapper = shallowMount(Counter);
+//     wrapper.find('button').trigger('click');
+//     expect(wrapper.find('p#js-count').text()).toMatch('1');
+//   })
+// })
