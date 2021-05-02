467d65db7f90b870bd5c37dec8b2476a6d53bab4
im <poson_hemp@yahoo.co.jp>
2021-05-02 16:36:07 +0900

    wrap by section tag

--- src/axios_sample/AxiosSample.vue
+++ src/axios_sample/AxiosSample.vue
@@ -1,28 +1,30 @@
 <template>
   <div>
     <h1>Bitcoin Price Index</h1>
-    <ul>
-      <div
-        v-for="item in items"
-        v-bind:key="item"
-        class="item"
-      >
-        <li>
-          <strong>
-            {{ item.description }}
-          </strong>
-          <span class="lighten">
-            <span v-html="item.symbol"></span>
-            {{ item.rate_float | currencyDecimal }}
-          </span>
-        </li>
+    <section>
+      <ul>
+        <div
+          v-for="item in items"
+          v-bind:key="item"
+          class="item"
+        >
+          <li>
+            <strong>
+              {{ item.description }}
+            </strong>
+            <span class="lighten">
+              <span v-html="item.symbol"></span>
+              {{ item.rate_float | currencyDecimal }}
+            </span>
+          </li>
+        </div>
+      </ul>
+      <hr>
+      <div>
+        <p><strong>raw data</strong></p>
+        {{ items }}
       </div>
-    </ul>
-    <hr>
-    <div>
-      <p><strong>raw data</strong></p>
-      {{ items }}
-    </div>
+    </section>
   </div>
 </template>
 
@@ -33,13 +35,22 @@ export default {
   name: 'AxiosSample',
   data() {
     return {
-      items: null
+      items: null,
+      loading: true,
+      errored: false
     }
   },
   mounted() {
     axios
     .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-    .then(response => (this.items = response.data.bpi))
+    .then(response => {this.items = response.data.bpi})
+    .catch(error => {
+      console.log(error)
+      this.errored = true
+    })
+    .finally(() => {
+      this.loading = false
+    })
   },
   filters: {
     currencyDecimal(value) {
