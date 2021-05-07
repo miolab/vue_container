c80f888a15102baa89b4800e5d84374df24c711c
im <poson_hemp@yahoo.co.jp>
2021-05-07 19:28:51 +0900

    sample: v-on:click (increment number function)

--- src/Counter.vue
+++ src/Counter.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <p>{{ count }}</p>
-    <button @click="increment">oshitara-kazuga-fuemasu</button>
+    <div>
+      <p>例（@click） {{ count }}</p>
+      <button @click="increment">oshitara-kazuga-fuemasu</button>
+    </div>
+    <div>
+      <p>例（v-on:click） {{ number }}</p>
+      <button v-on:click="countUp">Count Up</button>
+    </div>
   </div>
 </template>
 
@@ -10,12 +16,16 @@ export default {
   name: 'Counter',
   data() {
     return {
-      count: 0
+      count: 0,
+      number: 0,
     }
   },
   methods: {
     increment() {
       this.count++
+    },
+    countUp: function() {
+      this.number += 1
     }
   }
 }
