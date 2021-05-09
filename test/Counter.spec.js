0de084a626ea35a60b7ff2d33ab82480d82cb495
im <poson_hemp@yahoo.co.jp>
2021-05-09 19:00:09 +0900

    add tests of functions: countUp(times), resetCount

--- test/Counter.spec.js
+++ test/Counter.spec.js
@@ -13,6 +13,17 @@ describe('Counter.vue', () => {
   it('increments count when button is clicked', async () => {
     await wrapper.find('button.btn-increment').trigger('click');
 
-    await expect(wrapper.find('div').text()).toMatch('1');
+    await expect(wrapper.find('span.count-int').text()).toMatch('1');
+  })
+
+  it('count up when button is clicked', async () => {
+    await wrapper.find('button.btn-countup-single').trigger('click');
+    await expect(wrapper.find('span.number-int').text()).toMatch('1');
+
+    await wrapper.find('button.btn-countup-twice').trigger('click');
+    await expect(wrapper.find('span.number-int').text()).toMatch('3');
+
+    await wrapper.find('button.btn-reset-count').trigger('click');
+    await expect(wrapper.find('span.number-int').text()).toMatch('0');
   })
 })
