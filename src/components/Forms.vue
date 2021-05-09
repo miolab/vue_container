1aa3fa4b1f0d721ec75748e07cce08f9ec3bfee1
im <poson_hemp@yahoo.co.jp>
2021-05-10 08:37:33 +0900

    prepare instance of Forms

---
+++ src/components/Forms.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="forms">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Forms',
+  data() {
+    return {
+      message: 'テストメッセージ'
+      // count: 0,
+      // number: 0,
+      // isMouseover: false,
+    }
+  }
+  // },
+//   methods: {
+//     increment() {
+//       this.count++
+//     },
+//     countUp: function(times) {
+//       this.number += 1 * times
+//     },
+//     resetCount: function() {
+//       this.number = 0
+//       this.count = 0
+//     },
+//     showDescription() {
+//       this.isMouseover = true
+//     },
+//     removeDescription() {
+//       this.isMouseover = false
+//     }
+//   }
+}
+</script>
