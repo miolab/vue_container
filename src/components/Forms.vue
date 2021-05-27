8461e2d1089508864fb89be3493ed8c81d0a32ab
im <poson_hemp@yahoo.co.jp>
2021-05-27 20:53:47 +0900

    feature: text form with text limit validation (computed)

--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="forms">
+    <p>フォーム</p>
     <input
       class="input-text"
       type="text"
@@ -7,6 +8,16 @@
     >
     <p>入力結果:
       <span class="show-input-text">{{ message }}</span>
+    </p>
+    <p>バリデーション付きフォーム（v-model.Lazy）</p>
+    <input
+      type="text"
+      v-model.lazy="messageByLazy"
+    >
+    <span>（{{ maxTextLimit }}文字以内）</span>
+    <p>入力結果:
+      <span class="show-input-text">{{ messageByLazy }}</span>
+      <span style="color: red;"> {{ messageLimitAlert }}</span>
     </p>
   </div>
 </template>
@@ -16,7 +27,16 @@ export default {
   name: 'Forms',
   data() {
     return {
-      message: ''
+      message: '',
+      messageByLazy: '',
+      maxTextLimit: 8
+    }
+  },
+  computed: {
+    messageLimitAlert() {
+      return this.messageByLazy.length >= this.maxTextLimit ?
+        String(this.maxTextLimit) + '文字未満にしてください' :
+        ''
     }
   }
 }
