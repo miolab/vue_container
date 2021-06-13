e79fc51193c73824a6e8aec6e654261ba480d7dd
im <poson_hemp@yahoo.co.jp>
2021-06-14 08:48:12 +0900

    add filter (global)

--- src/components/axios_sample/AxiosSampleHeader.vue
+++ src/components/axios_sample/AxiosSampleHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <h1>Bitcoin Price Index</h1>
+    <h1>{{ titleHeader | myFilterUpperCase }}</h1>
     <p>
       {{ headerDescription }}
       <span style="font-size: 0.8em;">（←{{ textLength }}文字）</span>
@@ -10,6 +10,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      titleHeader: 'Bitcoin Price Index'
+    }
+  },
   props: {
     headerDescription: String
   },
